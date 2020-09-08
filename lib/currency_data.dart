0bfed56320cf9f5e67ef03e3cc8d333d0fae7668
Sandeep Dimri <sandeep1dimri@gmail.com>
2020-09-07 23:59:33 -0400

    Service call added, its scalable based on crypto currencies in the list

--- lib/currency_data.dart
+++ lib/currency_data.dart
@@ -1,3 +1,41 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 const List<String> currencyType = ['CAD', 'EUR', 'GBP', 'RS', 'USD'];
 
 const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
+const String _apikey = 'BE4368EF-8876-4FC4-B0E7-864F49472B919';
+const String _baseURL = 'https://rest.coinapi.io/v1/exchangerate/';
+
+class CryptoToRegular {
+  var _cryptoToReg = {};
+
+  CryptoToRegular() {
+    for (String eachCryptoType in cryptoList) {
+      _cryptoToReg[eachCryptoType] = '?';
+    }
+  }
+
+  dynamic convertCryptoToRegularBP() {
+    return _cryptoToReg;
+  }
+
+  dynamic convertCryptoToRegular({String currencyType}) async {
+    for (String eachCryptoType in cryptoList) {
+      String url =
+          _baseURL + eachCryptoType + '/' + currencyType + '?apikey=$_apikey';
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        var conversionRate = jsonResponse['rate'];
+        double conversionRateDouble = double.parse(conversionRate.toString());
+
+        _cryptoToReg[eachCryptoType] = conversionRateDouble.toStringAsFixed(2);
+      } else
+        _cryptoToReg[eachCryptoType] = "NF" + response.statusCode.toString();
+    }
+    print("****************+++++++++");
+    print(_cryptoToReg);
+    return _cryptoToReg;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -5,9 +5,6 @@ import 'dart:io' show Platform; // or hide if need any class
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-const String apikey = 'BE4368EF-8876-4FC4-B0E7-864F49472B919';
-const String baseURL = 'https://rest.coinapi.io/v1/exchangerate/';
-
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
@@ -16,6 +13,16 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   String dropDownSelectedValue = 'USD';
   String cadValue = '?';
+  var _cryptoToRegConversion = {};
+  CryptoToRegular cryptoToReg = CryptoToRegular();
+  _PriceScreenState() {
+    print("Construct!!!!");
+    _cryptoToRegConversion = cryptoToReg.convertCryptoToRegularBP();
+  }
+  void convertCryptoToRegular() async {
+    _cryptoToRegConversion = await cryptoToReg.convertCryptoToRegular(
+        currencyType: dropDownSelectedValue);
+  }
 
   DropdownButton<String> buildDropDownListAndroid() {
     List<DropdownMenuItem<String>> dropDownList = [];
@@ -31,12 +38,12 @@ class _PriceScreenState extends State<PriceScreen> {
       value: dropDownSelectedValue,
       items: dropDownList,
       onChanged: (value) async {
-        var rateValue =
-            await conversionRate(cryptoCurr: "BTC", regularCurrency: value);
-        print(rateValue);
+        _cryptoToRegConversion =
+            await cryptoToReg.convertCryptoToRegular(currencyType: value);
+        //print(rateValue);
         setState(() {
           this.dropDownSelectedValue = value;
-          this.cadValue = rateValue;
+          //this.cadValue = rateValue;
         });
       },
     );
@@ -59,31 +66,19 @@ class _PriceScreenState extends State<PriceScreen> {
       children: dropDownListiOS, //[Text('USD'), Text('CAD')],
       onSelectedItemChanged: (selectedIndex) async {
         //print(currencyType[selectedIndex]);
-        var rateValue = await conversionRate(
-            cryptoCurr: "BTC", regularCurrency: currencyType[selectedIndex]);
+        // var rateValue = await conversionRate(
+        //     cryptoCurr: "BTC", regularCurrency: currencyType[selectedIndex]);
+        _cryptoToRegConversion = await cryptoToReg.convertCryptoToRegular(
+            currencyType: currencyType[selectedIndex]);
         setState(() {
-          this.cadValue = rateValue;
+          //this.cadValue = rateValue;
+          this.dropDownSelectedValue = currencyType[selectedIndex];
         });
       },
     );
   }
 
-  Future<String> conversionRate(
-      {String cryptoCurr, String regularCurrency}) async {
-    String url =
-        baseURL + cryptoCurr + '/' + regularCurrency + '?apikey=$apikey';
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      var conversionRate = jsonResponse['rate'];
-      double conversionRateDouble = double.parse(conversionRate.toString());
-
-      return conversionRateDouble.toStringAsFixed(2);
-    } else
-      return "NF" + response.statusCode.toString();
-  }
-
-  List<Padding> cardsOnScreen() {
+  List<Padding> cardsOnScreen({String crypoCurr}) {
     List<Padding> cardsOnScreen = [];
     for (String crypoType in cryptoList) {
       Padding newCard = Padding(
@@ -96,7 +91,11 @@ class _PriceScreenState extends State<PriceScreen> {
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
             child: Text(
-              '1 BTC= $cadValue $crypoType',
+              //'1 $crypoType= $cadValue $crypoCurr',
+              '1 $crypoType= ' +
+                  _cryptoToRegConversion[crypoType] +
+                  ' ' +
+                  crypoCurr,
               style: TextStyle(fontSize: 20, color: Colors.white),
               textAlign: TextAlign.center,
             ),
@@ -118,23 +117,8 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
-            child: Card(
-              color: Colors.lightBlue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
-                child: Text(
-                  '1 BTC= $cadValue CAD',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
+          for (Padding each in cardsOnScreen(crypoCurr: dropDownSelectedValue))
+            each,
           Container(
             height: 150,
             padding: EdgeInsets.only(bottom: 30),
@@ -143,16 +127,6 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Platform.isIOS
                 ? buildDropDownListiOS()
                 : buildDropDownListAndroid(),
-            /* implement ios level changes
-            child: DropdownButton<String>(
-              value: dropDownSelectedValue,
-              items: buildDropDownList(),
-              onChanged: (value) {
-                setState(() {
-                  dropDownSelectedValue = value;
-                });
-              },
-            ),*/
           )
         ],
       ),
